ef4bc46de5411295a4af7df2bc9b9a8be57b5bba
AnasKhan20 <143949011+AnasKhan20@users.noreply.github.com>
2023-10-05 00:49:06 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,101 @@
+// chapter 1
+
+// Question 1
+// alert("First Name")
+// alert("Last Name")
+// alert("Email")
+// alert("Phone Number")
+// alert("Password")
+
+// Question2
+// alert("You are learning JavaScript!")
+
+// Question3
+// alert("I am learning JavaScript")
+
+
+// Chapter 2
+
+// Question 1
+// var firstName;
+
+// Question 2
+// var lastName;
+// lastName= Khan
+
+// Question 3
+// var teamName;
+// teamName= "Strikers"
+// alert(teamName)
+
+// Question 4
+// var bestMan = "Anas"
+// alert(bestMan)
+
+
+// Chapter 3
+
+// Question 1
+// var caseQty;
+
+// Question 2
+// var caseQty = 144;
+
+// Question 3
+// var num = 9;
+
+// Question 4
+// var num;
+// num = 2 + 2
+
+// Question 5
+// var merchTotal = 100;
+// var shippingCharge = 10;
+// var orderTotal = merchTotal + shippingCharge;
+// alert(orderTotal)
+
+// Question 6
+// var num = 10;
+// num = 10 + 20;
+// alert(num)
+
+// Chapter 4
+
+// Question 1
+// var productCost = 3.45;
+
+// Question 2
+// var nameofBand;
+
+// Question 3
+// var num = 30;
+
+// Question 4
+// var firstName = "Anas"
+// var lastName = "khan"
+
+// Question 5
+// legal      illegal
+// num        if 
+// text       add
+// name
+
+
+// Chapter 5
+
+
+// Quetion 1
+// remaider operator = %
+
+// Question 2
+// 2
+
+// Question 3
+// var largeNum = 1000 * 2000;
+
+// // Question 6
+// var num = 6;
+// num = 6 * 2
+// alert(num)
+
+
